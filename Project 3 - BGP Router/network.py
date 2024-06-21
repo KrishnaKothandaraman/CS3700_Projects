7d3d06adec57a4dc78f0fe5773cc99072324bff1
KrishnaKothandaraman <kkrishna0701@gmail.com>
2024-06-21 19:06:48 +0800

    Initial bgp redo

--- Project 3 - BGP Router/network.py
+++ Project 3 - BGP Router/network.py
@@ -1,99 +1,10 @@
-import copy
-from dataclasses import dataclass
-from enum import Enum, auto
-from typing import List, Optional, Tuple
-import ip
 
 
 class Network:
-    peer: str
-    network: str
-    netmask: str
-    localpref: int
-    selfOrigin: bool
-    ASPath: List[int]
-    origin: str
-
-    def __init__(self, update_message):
-        object.__setattr__(self, 'peer', (update_message["src"]))
-        object.__setattr__(self, 'network', (update_message["msg"]["network"]))
-        object.__setattr__(self, 'netmask', (update_message["msg"]["netmask"]))
-        object.__setattr__(self, 'localpref', update_message["msg"]["localpref"])
-        object.__setattr__(self, 'selfOrigin', update_message["msg"]["selfOrigin"])
-        object.__setattr__(self, 'ASPath', update_message["msg"]["ASPath"])
-        object.__setattr__(self, 'origin', update_message["msg"]["origin"])
-
-    def __lt__(self, other):
-        if ip.cidr_length(self.netmask) > ip.cidr_length(other.netmask):
-            return True
-        elif ip.cidr_length(self.netmask) == ip.cidr_length(other.netmask) and self.localpref > other.localpref:
-            return True
-        elif ip.cidr_length(self.netmask) == ip.cidr_length(other.netmask) and self.localpref == other.localpref \
-                and (self.selfOrigin is True and other.selfOrigin is not True):
-            return True
-        elif ip.cidr_length(self.netmask) == ip.cidr_length(other.netmask) and self.localpref == other.localpref \
-                and (self.selfOrigin == other.selfOrigin) and len(self.ASPath) < len(other.ASPath):
-            return True
-        elif ip.cidr_length(self.netmask) == ip.cidr_length(other.netmask) and self.localpref == other.localpref \
-                and (self.selfOrigin == other.selfOrigin) and len(self.ASPath) == len(other.ASPath) \
-                and self.compareOrigins(other) == 1:
-            return True
-        elif ip.cidr_length(self.netmask) == ip.cidr_length(other.netmask) and self.localpref == other.localpref \
-                and (self.selfOrigin == other.selfOrigin) and len(self.ASPath) == len(other.ASPath) \
-                and self.compareOrigins(other) == 0 and ip.compareIP(self.peer, other.peer):
-            return True
-        else:
-            return False
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def __repr__(self):
-        return f"{self.__dict__}"
-
-    def have_same_attributes(self, other) -> bool:
-        """
-        Returns True if two networks have same attributes
-        :param other: Other network
-        :return:
-        """
-        return self.peer == other.peer and self.netmask == other.netmask and self.localpref == other.localpref \
-            and (self.selfOrigin == other.selfOrigin) and len(self.ASPath) == len(other.ASPath) \
-            and self.compareOrigins(other) == 0
-
-    def serialize(self) -> dict:
-        """
-        Method returns a dictionary representation of the entire class. Useful for dump message
-        :return: dictionary representation of this class
-        """
-        return self.__dict__
-
-    def serialize_for_forwarding(self, asn: int) -> dict:
-        """
-        Call this method to return dictionary for forwarding update message to neighbours
-        :return: dictionary representation of non private fields of this network
-        """
-        return {"network": self.network, "netmask": self.netmask, "ASPath": [asn] + self.ASPath}
-
-    def prefix_match(self, ipaddr) -> bool:
-        """
-        Call this method to check if an IPaddress is in a network
-        :param ipaddr: str representation of IP Address
-        :return: boolean representing whether this network has this IP address
-        """
-        CIDR = ip.cidr_length(self.netmask)
-        return ip.tobin(self.network)[:CIDR] == ip.tobin(ipaddr)[:CIDR]
-
-    def compareOrigins(self, other) -> int:
-        """
-        C-style comparison operator. 0 implies equals, 1 implies self > other and -1 implies self < other
-        :param other: Instance of type Network
-        :return: int
-        """
-        if self.origin == other.origin:
-            return 0
-        elif self.origin == "IGP" and other.origin != "IGP":
-            return 1
-        elif self.origin == "EGP" and other.origin == "UNK":
-            return 1
-        return -1
+    def __init__(self, network, netmask, localpref, selfOrigin, ASPath, origin):
+        self.network = network
+        self.netmask = netmask
+        self.localpref = localpref
+        self.selfOrigin = selfOrigin
+        self.ASPath = ASPath
+        self.origin = origin
--- Project 3 - BGP Router/table.py
+++ Project 3 - BGP Router/table.py
@@ -1,119 +1,16 @@
-import copy
 
+from typing import Dict, List
 from network import Network
-from typing import Dict, List, Tuple, Optional, Set
-import ip
 
 
-# represents the routing table
+
 class Table:
-    routing_table: List[Tuple[Network, str]]
-    aggregate_result: List[Tuple[Network, Tuple[Network, Network]]]
-    aggregated_networks: Set[Network]
 
     def __init__(self):
-        self.routing_table = []
-        self.aggregate_result = []
-        self.aggregated_networks = set()
-
-    # add an entry to the table with given message
-    def add_entry(self, message):
-        neighbor_ip = message["src"]
-        network = Network(message)
-
-        # just in case the neighbor changes its "network-netmask" fields in msg
-        self.routing_table.append((network, neighbor_ip))
-        self.aggregate()
-
-    def get_adjacent_networks(self, other: Network, aggregation_map: Dict[int, int]) -> Optional[int]:
-        """
-        Return list of indices of networks from routing table that are adjacent to a given network
-        :param aggregation_map: Dictionary of already processed networks
-        :param other: Network instance
-        :return: List[int]
-        """
-        for i, (network, neighbour) in enumerate(self.routing_table):
-            if i in aggregation_map or network == other:
-                continue
-            if ip.are_adjacent(other.network, other.netmask, network.network, network.netmask):
-                return i
-
-    def aggregate(self) -> None:
-        """
-        aggregate the routing_table if possible
-        :return: None
-        """
-        aggregation_map: Dict[int, int] = {}
-        for i, (network, neighbour) in enumerate(self.routing_table):
-            adjacent_network = self.get_adjacent_networks(network, aggregation_map)
-            if not adjacent_network:
-                continue
-            aggregate_network = self.get_aggregate_networks(network, adjacent_network)
-            if not aggregate_network:
-                continue
-            aggregation_map[i] = aggregate_network
-
-        for network, adjacent_network_index in aggregation_map.items():
-            if not adjacent_network_index:
-                continue
-            original_network = copy.deepcopy(self.routing_table[network][0])
-            adjacent_network = self.routing_table[adjacent_network_index][0]
-
-            self.routing_table.pop(adjacent_network_index)
-
-            self.routing_table[network][0].network = ip.aggregate_network(self.routing_table[network][0].network,
-                                                                          self.routing_table[network][0].netmask)
-            self.routing_table[network][0].netmask = ip.aggregate_netmask(self.routing_table[network][0].netmask)
-
-            self.aggregate_result.append((self.routing_table[network][0], (original_network, adjacent_network)))
-
-        if len(aggregation_map) > 0:
-            self.aggregate()
-
-    def get_aggregate_networks(self, network, adjacent_network) -> Optional[int]:
-        """
-        Removes adjacent networks that cannot be aggregated with network
-        :param network: Base network
-        :param adjacent_network: index of adjacent network from routing table
-        :return: List[int]
-        """
-        for i, (peer_network, neighbour) in enumerate(self.routing_table):
-            if i != adjacent_network:
-                continue
-            if not network.have_same_attributes(peer_network):
-                return None
-        return adjacent_network
-
-    def rebuild_table(self, update_messages) -> None:
-        """
-        Resets routing table and rebuilds it with the list of update messages provided
-        :param update_messages: List of update messages
-        :return: None
-        """
-        self.routing_table = []
-
-        for message in update_messages:
-            self.add_entry(message)
-
-        self.aggregate()
-
-    def find_route(self, destination: str) -> List[Network]:
-        """
-        checking if the given destination is in the table or not;
-        if it is returns the corresponding neighbor's IP.
-        :param destination: IP
-        :return: neighbor's IP
-        """
-        valid_networks = []
-        for network, neighbour in self.routing_table:
-            can_route = network.prefix_match(destination)
-            if can_route:
-                valid_networks.append(network)
-        return valid_networks
-
-    def get_serialized_table(self) -> List:
-        """
-        Returns serialized representation of forwarding table
-        :return: List of dictionaries of the forwarding table
-        """
-        return list(map(lambda x: x[0].serialize(), self.routing_table))
+        self.networkMap : Dict[str , List[Network]] = {}
+    
+    def add_network(self, network_addr: str, network: Network):
+        if network_addr in self.networkMap:
+            self.networkMap[network_addr].append(network)
+        else:
+            self.networkMap[network_addr] = [network]
